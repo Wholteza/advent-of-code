7c118fc24f41f41160d3f11204c2b9bb561b2b67
Zackarias Montell <zackarias@montell.se>
2020-12-06 10:16:54 +0100

    Removed comment

--- 2020/3b/run.ts
+++ 2020/3b/run.ts
@@ -2,13 +2,6 @@ import { map } from "./parsed-input";
 
 const TREE = "#";
 
-// Right 1, down 1.
-// Right 3, down 1. (This is the slope you already checked.)
-// Right 5, down 1.
-// Right 7, down 1.
-// Right 1, down 2.
-// multiply all trees together
-
 const bottomOfSlope = map.length - 1;
 const getLoopedXPosition = (x: number) => x % map[0].length;
 const treeWasHit = (x: number, y: number): boolean => map[y][x] === TREE;
