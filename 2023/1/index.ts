9c051443788208f40809b83bdd7132c3ef2c23d7
Zackarias Montell <zackarias.montell@revolutionrace.se>
2023-12-01 17:39:52 +0100

    wip 1b

--- 2023/1/index.ts
+++ 2023/1/index.ts
@@ -24,15 +24,65 @@ const getSumOfNumbersFromEachRow = (input: string): number =>
     .map(getTwoDigitNumberOfRow)
     .reduce((sum, current) => sum + current, 0);
 
-const testResultsAreNotCorrect = (): boolean => {
+const partOneTestResultsAreNotCorrect = (): boolean => {
   const testResultsFromExercise = 142;
-  const result = getSumOfNumbersFromEachRow(readContentsOfFile("test-input"));
+  const result = getSumOfNumbersFromEachRow(readContentsOfFile("test-input-a"));
   return result !== testResultsFromExercise;
 };
 
 const calculateResult = (): number =>
   getSumOfNumbersFromEachRow(readContentsOfFile("input"));
 
-if (testResultsAreNotCorrect()) throw new Error("Test results are not correct");
+if (partOneTestResultsAreNotCorrect())
+  throw new Error("Test results are not correct");
 console.log("Test results are correct");
 console.log(`Result is ${calculateResult()}`);
+
+console.log("End of part one\n\nStart of part two");
+
+const turnLettersIntoNumbers = (input: string): string => {
+  const replacements: { match: string; replaceWith: number }[] = [
+    { match: "one", replaceWith: 1 },
+    { match: "two", replaceWith: 2 },
+    { match: "three", replaceWith: 3 },
+    { match: "four", replaceWith: 4 },
+    { match: "five", replaceWith: 5 },
+    { match: "six", replaceWith: 6 },
+    { match: "seven", replaceWith: 7 },
+    { match: "eight", replaceWith: 8 },
+    { match: "nine", replaceWith: 9 },
+  ];
+  let lettersLeft = input;
+  let newRow = "";
+  while (lettersLeft.length) {
+    newRow += lettersLeft[0];
+    lettersLeft = lettersLeft.slice(1);
+    replacements.forEach(
+      (replacement) =>
+        (newRow = newRow.replace(
+          replacement.match,
+          replacement.replaceWith.toString()
+        ))
+    );
+    if (!lettersLeft) break;
+  }
+  return newRow;
+};
+
+const calculateResultOfPartTwo = (fileName: string): number =>
+  toArrayOfRows(readContentsOfFile(fileName))
+    .map(turnLettersIntoNumbers)
+    .map(getTwoDigitNumberOfRow)
+    .reduce((sum, current) => sum + current, 0);
+
+const partTwoTestResultsAreNotCorrect = (): boolean => {
+  const testResultsFromExercise = 281;
+  const result = calculateResultOfPartTwo("test-input-b");
+  console.log(result, testResultsFromExercise);
+  return result !== testResultsFromExercise;
+};
+
+if (partTwoTestResultsAreNotCorrect())
+  throw new Error("Test results are not correct");
+console.log("Test results are correct");
+console.log(`Result is ${calculateResultOfPartTwo("input")}`);
