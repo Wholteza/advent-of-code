d270ef2ca242e52f23d399177e25961a8737f070
wholteza <zackarias@montell.se>
2023-12-03 22:31:38 +0100

    3b

--- 2023/3/index.ts
+++ 2023/3/index.ts
@@ -1,4 +1,3 @@
-import { strictEqual } from "assert";
 import fs from "fs";
 
 const readRowsOfFile = (fileName: string): string[] =>
@@ -7,8 +6,8 @@ const readRowsOfFile = (fileName: string): string[] =>
     .split("\n")
     .filter((row) => !!row);
 
-type Coordinates = { x: number; y: number };
-type Square = { 1: Coordinates; 2: Coordinates; partNumber: number };
+type Coordinate = { x: number; y: number };
+type Square = { 1: Coordinate; 2: Coordinate; partNumber: number };
 type Row = string[];
 type Region = string[][];
 
@@ -53,12 +52,16 @@ const toSquareWithSurroundings = (square: Square): Square => {
   return squareWithSurroundings;
 };
 
-const identifyAreasOfPartNumbers = (
+const identifyAreasOfPartNumbers = (row: string, rowIndex: number): Square[] =>
+  identifyArea(row, rowIndex, /(\d+)/g);
+
+const identifyArea = (
   row: string,
-  rowIndex: number
+  rowIndex: number,
+  pattern: RegExp
 ): Square[] => {
   const squares: Square[] = [];
-  for (let match of row.matchAll(/(\d+)/g)) {
+  for (const match of row.matchAll(pattern)) {
     if (!match || match.index === undefined || !match[0])
       throw new Error("Could not find area of part number");
     squares.push({
@@ -100,3 +103,88 @@ const runPartOneTest = () => {
 runPartOneTest();
 const result = calculatePartOneResultFromFile("input");
 console.log(`Result is: ${result}`);
+class PartNumber {
+  square: Square;
+  number: number;
+  constructor(square: Square) {
+    this.square = square;
+    this.number = square.partNumber;
+  }
+
+  getCollisionSquare = () => toSquareWithSurroundings(this.square);
+
+  getRegionSurrounding = (schematic: Region) =>
+    getRegionOfSquare(toSquareWithSurroundings(this.square), schematic);
+}
+
+class Cog {
+  coordinate: Coordinate;
+  constructor(coordinate: Coordinate) {
+    this.coordinate = coordinate;
+  }
+
+  getSquare = (): Square => ({
+    "1": { x: this.coordinate.x, y: this.coordinate.y },
+    "2": { x: this.coordinate.x, y: this.coordinate.y },
+    partNumber: 0,
+  });
+
+  getAreaSurrounding = (schematic: Region) =>
+    getRegionOfSquare(toSquareWithSurroundings(this.getSquare()), schematic);
+
+  isColliding = (part: PartNumber, schematic: Region) => {
+    const areaSurroundingPart = part.getCollisionSquare();
+    const colliding =
+      areaSurroundingPart[1].x <= this.coordinate.x &&
+      areaSurroundingPart[2].x >= this.coordinate.x &&
+      areaSurroundingPart[1].y <= this.coordinate.y &&
+      areaSurroundingPart[2].y >= this.coordinate.y;
+
+    return colliding;
+  };
+
+  getGearRatio = (parts: PartNumber[], schematic: Region): number => {
+    const connectedParts = parts.filter((p) => this.isColliding(p, schematic));
+    if (connectedParts.length !== 2) return 0;
+    return connectedParts[0].number * connectedParts[1].number;
+  };
+}
+class EngineSchematic {
+  schematic: Region;
+  partNumbers: PartNumber[];
+  cogs: Cog[];
+  constructor(fileName: string) {
+    const rows = readRowsOfFile(fileName);
+    this.schematic = rows.map((row) => row.split(""));
+    this.partNumbers = rows
+      .flatMap(identifyAreasOfPartNumbers)
+      .map((s) => new PartNumber(s));
+    this.cogs = this.identifyAreasOfCogs(rows).map(
+      (c) => new Cog({ x: c[1].x, y: c[1].y })
+    );
+  }
+
+  private identifyAreasOfCogs = (rows: string[]): Square[] =>
+    rows.flatMap((row, index) => identifyArea(row, index, /(\*)/g));
+
+  public getSumOfCogsConnectingTwoParts = () =>
+    this.cogs.reduce(
+      (sum, cog) => sum + cog.getGearRatio(this.partNumbers, this.schematic),
+      0
+    );
+}
+
+const calculatePartTwoResultFromFile = (fileName: string): number =>
+  new EngineSchematic(fileName).getSumOfCogsConnectingTwoParts();
+
+const runPartTwoTest = () => {
+  const expected = 467835;
+  const result = calculatePartTwoResultFromFile("test-input-a");
+  if (result !== expected)
+    throw new Error(`Expected ${expected}, got ${result}`);
+  console.log("Test passed!");
+};
+
+runPartTwoTest();
+const resultTwo = calculatePartTwoResultFromFile("input");
+console.log(`Result is: ${resultTwo}`);
