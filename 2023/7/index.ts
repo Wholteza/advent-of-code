166a2a645fd16c0f6002895bbd21b7854a5d40d9
wholteza <zackarias@montell.se>
2023-12-17 22:13:39 +0100

    wip 7b

--- 2023/7/index.ts
+++ 2023/7/index.ts
@@ -1,6 +1,7 @@
+import { count } from "console";
 import fs, { appendFile } from "fs";
 
-const powerMap = {
+const powerMapV1 = {
   "High Card": 0,
   2: 1,
   3: 2,
@@ -31,7 +32,7 @@ type HandType =
   | "Full House"
   | "Four of a Kind"
   | "Five of a kind";
-type Card = keyof Omit<typeof powerMap, HandType>;
+type Card = keyof Omit<typeof powerMapV1, HandType>;
 type Cards = Card[];
 
 type Hand = { bid: number; hand: Cards };
@@ -48,7 +49,7 @@ const readHands = (filename: string): Hands =>
       return { bid: +bid, hand: hand.split("") as Cards };
     });
 
-const toType = (hand: Hand): HandType => {
+const toTypeV1 = (hand: Hand): HandType => {
   const cardCountsRecord = hand.hand.reduce((acc, card) => {
     if (!acc[card]) {
       acc[card] = 1;
@@ -75,28 +76,31 @@ const toType = (hand: Hand): HandType => {
   return "High Card";
 };
 
-const byPower = (a: Hand, b: Hand): number => {
-  const aType = toType(a);
-  const bType = toType(b);
-  const aPower = powerMap[aType];
-  const bPower = powerMap[bType];
+const byPower = (
+  a: Hand,
+  b: Hand,
+  map: typeof powerMapV1 | typeof powerMapV2,
+  toHandType: (hand: Hand) => HandType
+): number => {
+  const aType = toHandType(a);
+  const bType = toHandType(b);
+  const aPower = map[aType];
+  const bPower = map[bType];
   const types = [aType, bType];
   if (types.every((type) => type === "High Card") || aType === bType) {
     for (let i = 0; i < 5; i++) {
       if (a.hand[i] === b.hand[i]) continue;
-      return powerMap[a.hand[i]] > powerMap[b.hand[i]] ? 1 : -1;
+      return map[a.hand[i]] > map[b.hand[i]] ? 1 : -1;
     }
   }
   return aPower === bPower ? 0 : aPower > bPower ? 1 : -1;
 };
-// figure out their type
-// compare hands
-// give them ranks
-// calculate score
 
 const daySevenPartOneResults = (filename: string): number => {
   const hands = readHands(filename);
-  const handsSortedByPower = hands.sort(byPower);
+  const handsSortedByPower = hands.sort((a, b) =>
+    byPower(a, b, powerMapV1, toTypeV1)
+  );
   const totalWinnings = handsSortedByPower.reduce(
     (acc, hand, index) => acc + hand.bid * (index + 1),
     0
@@ -114,3 +118,88 @@ const daySevenPartOneTest = () => {
 
 daySevenPartOneTest();
 console.log("The result is: ", daySevenPartOneResults("input"));
+
+const powerMapV2 = {
+  "High Card": 0,
+  J: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  T: 10,
+  Q: 11,
+  K: 12,
+  A: 13,
+  "One Pair": 14,
+  "Two Pair": 15,
+  "Three of a Kind": 16,
+  "Full House": 17,
+  "Four of a Kind": 18,
+  "Five of a kind": 19,
+} as const;
+
+const toTypeV2 = (hand: Hand): HandType => {
+  const cardCountsRecord = hand.hand.reduce((acc, card) => {
+    if (!acc[card]) {
+      acc[card] = 1;
+    } else {
+      acc[card]++;
+    }
+    return acc;
+  }, {} as Record<Card, number>);
+  const jCount = cardCountsRecord.J ?? 0;
+  const cardCounts = Object.keys(cardCountsRecord)
+    .filter((key) => key !== "J")
+    .map((key) => ({
+      card: key,
+      count: cardCountsRecord[key as Card],
+    }));
+  if (cardCounts.some(({ count }) => count === 3)) {
+    if (jCount === 2) return "Five of a kind";
+    if (jCount === 1) return "Four of a Kind";
+    return "Three of a Kind";
+  }
+  if (cardCounts.some(({ count }) => count === 2)) {
+    if (jCount === 3) return "Five of a kind";
+    if (jCount === 2) return "Four of a Kind";
+    if (jCount === 1) {
+      if (new Set(cardCounts.map(({ card }) => card)).size === 2)
+        return "Full House";
+      return "Three of a Kind";
+    }
+    return "One Pair";
+  }
+  if (jCount === 5) return "Five of a kind";
+  if (jCount === 4) return "Five of a kind";
+  if (jCount === 3) return "Four of a Kind";
+  if (jCount === 2) return "Three of a Kind";
+  if (jCount === 1) return "One Pair";
+  return "High Card";
+};
+
+const daySevenPartTwoResults = (filename: string): number => {
+  const hands = readHands(filename);
+  const handsSortedByPower = hands.sort((a, b) =>
+    byPower(a, b, powerMapV2, toTypeV2)
+  );
+  const totalWinnings = handsSortedByPower.reduce(
+    (acc, hand, index) => acc + hand.bid * (index + 1),
+    0
+  );
+  return totalWinnings;
+};
+
+const daySevenPartTwoTest = () => {
+  const expected = 5905;
+  const result = daySevenPartTwoResults("test-input-a");
+  if (result !== expected)
+    throw new Error(`Expected ${expected}, got ${result}.`);
+  console.log("Test passed!");
+};
+
+daySevenPartTwoTest();
+console.log("The result is: ", daySevenPartTwoResults("input"));
